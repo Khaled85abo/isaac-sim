168f295e7305203a156d49811baa385c76130cb0
Khaled Abo <36343726+Khaled85abo@users.noreply.github.com>
2024-09-05 13:40:18 +0200

    Omar training

---
+++ jetbot_move.py
@@ -0,0 +1,99 @@
+# Copyright (c) 2021-2023, NVIDIA CORPORATION. All rights reserved.
+#
+# NVIDIA CORPORATION and its licensors retain all intellectual property
+# and proprietary rights in and to this software, related documentation
+# and any modifications thereto. Any use, reproduction, disclosure or
+# distribution of this software and related documentation without an express
+
+import argparse
+
+from isaacsim import SimulationApp
+
+# Parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("--test", default=False, action="store_true", help="Run in test mode")
+args, unknown = parser.parse_known_args()
+
+# Livestream configuration settings
+CONFIG = {
+    "width": 1280,
+    "height": 720,
+    "window_width": 1920,
+    "window_height": 1080,
+    "headless": True,
+    "hide_ui": False,  # Show the GUI
+    "renderer": "RayTracedLighting",
+    "display_options": 3286,  # Set display options to show default grid
+}
+
+# Initialize the SimulationApp with livestream settings
+simulation_app = SimulationApp(launch_config=CONFIG)
+
+import carb
+import numpy as np
+from omni.isaac.core import World
+from omni.isaac.nucleus import get_assets_root_path
+from omni.isaac.wheeled_robots.controllers.differential_controller import DifferentialController
+from omni.isaac.wheeled_robots.robots import WheeledRobot
+
+from omni.isaac.core.utils.extensions import enable_extension
+
+# Enable livestream extensions
+simulation_app.set_setting("/app/window/drawMouse", True)
+simulation_app.set_setting("/app/livestream/proto", "ws")
+simulation_app.set_setting("/ngx/enabled", False)
+
+# Enable the necessary livestream extensions
+enable_extension("omni.kit.streamsdk.plugins-3.2.1")
+enable_extension("omni.kit.livestream.core-3.2.0")
+enable_extension("omni.kit.livestream.native")
+
+my_world = World(stage_units_in_meters=1.0)
+assets_root_path = get_assets_root_path()
+if assets_root_path is None:
+    carb.log_error("Could not find Isaac Sim assets folder")
+jetbot_asset_path = assets_root_path + "/Isaac/Robots/Jetbot/jetbot.usd"
+my_jetbot = my_world.scene.add(
+    WheeledRobot(
+        prim_path="/World/Jetbot",
+        name="my_jetbot",
+        wheel_dof_names=["left_wheel_joint", "right_wheel_joint"],
+        create_robot=True,
+        usd_path=jetbot_asset_path,
+        position=np.array([0, 0.0, 2.0]),
+    )
+)
+my_world.scene.add_default_ground_plane()
+my_controller = DifferentialController(name="simple_control", wheel_radius=0.03, wheel_base=0.1125)
+my_world.reset()
+
+i = 0
+reset_needed = False
+while simulation_app.is_running():
+    my_world.step(render=True)
+    if my_world.is_stopped() and not reset_needed:
+        reset_needed = True
+    if my_world.is_playing():
+        if reset_needed:
+            my_world.reset()
+            my_controller.reset()
+            reset_needed = False
+        if i >= 0 and i < 1000:
+            # forward
+            my_jetbot.apply_wheel_actions(my_controller.forward(command=[0.05, 0]))
+            print(my_jetbot.get_linear_velocity())
+        elif i >= 1000 and i < 1300:
+            # rotate
+            my_jetbot.apply_wheel_actions(my_controller.forward(command=[0.0, np.pi / 12]))
+            print(my_jetbot.get_angular_velocity())
+        elif i >= 1300 and i < 2000:
+            # forward
+            my_jetbot.apply_wheel_actions(my_controller.forward(command=[0.05, 0]))
+        elif i == 2000:
+            i = 0
+        i += 1
+    if args.test is True:
+        break
+
+
+simulation_app.close()
